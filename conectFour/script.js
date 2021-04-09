a76fa8ca810189e372613661ece4951fa482d614
Beni Mahat <benimahat6565@gmail.com>
2021-04-08 23:29:20 -0600

    chips drop

--- conectFour/script.js
+++ conectFour/script.js
@@ -1,12 +1,36 @@
 //selectors
-const tableRow = document.getElementsByTagName('tr');
-const tableCell = document.getElementsByTagName('td');
-const tableSlot = document.querySelector('.slot');
-const playerTurn = document.querySelector(".player_turn");
-const reset = document.querySelector(".reset");
+let tableRow = document.getElementsByTagName('tr');
+let tableCell = document.getElementsByTagName('td');
+let tableSlot = document.querySelector('.slot');
+let reset = document.querySelector(".reset");
 
 for(let i=0; i< tableCell.length; i++){
     tableCell[i].addEventListener('click', (e) => {
-        console.log(`row${e.target.parentElement.rowIndex}, col${e.target.cellIndex}`)
+        console.log(`row${e.target.parentElement.rowIndex}, col${e.target.cellIndex}`);
     })
+}
+
+let currentPlayer = 1;
+
+Array.prototype.forEach.call(tableCell, (cell) => {
+    cell.addEventListener("click", changeColor);
+    cell.style.backgroundColor = "white";
+})
+
+function changeColor(e){
+    let column = e.target.cellIndex;
+    let row = [];
+    for(let i=5; i > -1; i--){
+        if(tableRow[i].children[column].style.backgroundColor === "white"){
+            row.push(tableRow[i].children[column]);
+            console.log(row[0])
+            if(currentPlayer === 1){
+                row[0].style.backgroundColor = "red";
+                return currentPlayer = 2;
+            } else {
+                row[0].style.backgroundColor = "blue";
+                return currentPlayer = 1;
+            }
+        }
+    }
 }
